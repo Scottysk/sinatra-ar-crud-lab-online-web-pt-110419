040659be5d22ff2738e2dbc9d5b5939eaa2fdaf6
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-02 02:18:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,6 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do
     @article = Article.find(params[:id])
-    @article.update(title:)
+    @article.update(title: params)
   end
 end
